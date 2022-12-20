92b5224b46df1ab2b1816590c55f991a6833581e
Lucas Rocha Amorim <lucasramorim2021@gmail.com>
2022-12-20 15:43:18 -0300

    projetos de logica de programacao feitos em java

--- resources/views/edit.blade.php
+++ resources/views/edit.blade.php
@@ -14,6 +14,6 @@
     <input type="string" name="budget_amount" id="budget_amount" placeholder="Valor do orçamento" value="{{ $budget->budget_amount }}">
     <input type="string" name="seller_name" id="seller_name" placeholder="Nome Do vendedor..." value="{{ $budget->seller_name }}">
     <input type="string" name="client_name" id="client_name" placeholder="Nome Do Cliente..." value="{{ $budget->client_name }}">
-    <textarea name="description" id="description" cols="30" rows="5" placeholder="Descrição do orçamento">{{ $budget->description }}"</textarea>
+    <textarea name="description" id="description" cols="30" rows="5" placeholder="Descrição do orçamento">{{ $budget->description }}</textarea>
     <button type="submit">Salvar</button>
 </form>
